92ec273aa4942164f1b9e84b183f5a003e4ecc82
LALITNEW <100189691+LALITNEW@users.noreply.github.com>
2023-02-09 18:28:41 +0700

    commit

--- hello.py
+++ hello.py
@@ -1,3 +1,2 @@
 def hellow():
     return "hello world"
-
--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import unittest
 from hello import hellow
 
+
 class Testhellow(unittest.TestCase):
 
     def test_sum(self):
         calculation = hellow()
         self.assertEqual(calculation, "hello world")
-
-if __name__ == '__main__':
-    unittest.main()
